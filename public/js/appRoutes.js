aa4b53baf772f17a3e85132fb5a6d327dadbc65f
Austin Lawless <austindlawless@gmail.com>
2014-11-08 16:26:51 -0600

    got the html page set up with an angular nav bar

---
+++ public/js/appRoutes.js
@@ -0,0 +1,22 @@
+// public/js/appRoutes.js
+
+angular.module(appRoutes, []).config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+
+	$routeProvider
+
+		// home page
+		.when('/', {
+			templateUrl: 'views/home.html',
+			controller: 'MainController'
+		})
+
+		// nerds page that will use the NerdController
+		.when('/nerds', {
+			templateUrl: 'views/nerds.html',
+			controller: 'NerdController'
+		});
+
+	$locatoinProvider.html5Mode(true);
+
+}];
+
