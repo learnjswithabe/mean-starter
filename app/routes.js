af066f13df80b866cb4ceb381e42f9a225e2b2e1
Austin Lawless <austindlawless@gmail.com>
2014-11-08 13:55:10 -0600

    got mongoose connected, added a post for nerds

--- app/routes.js
+++ app/routes.js
@@ -11,17 +11,34 @@ module.exports = function(app) {
 
 	// sample api route
 	app.get('/api/nerds', function(req, res) {
+		console.log('GET /api/nerds');
 		// use mongoose to get all the nerds from the db
 		Nerd.find(function(err, nerds) {
+			console.log('find');
 			// if there is an error retreiving, send the error - nothing fter res.send(err) will execute
 			if (err) {
+				console.log('error');
 				res.send(err);
 			}
+			console.log('no error');
 			res.send(nerds);	// return all the nerds in JSON format
 		});
 	});
 
 	// route to handle createing goes here (app.post)
+	app.post('/api/nerds', function(req, res) {
+		console.log('POST /api/nerds');
+		var nerd = new Nerd();	// use mongoose to add a new nerd to the db
+		nerd.name = req.body.name
+		// save the nerd
+		nerd.save(function(err) {
+			if (err) {
+				res.send(err);
+			}
+			res.json({ message: 'Nerd (' + nerd.name + ') created!' });
+		});
+	});
+
 	// route to handle delete goes here (app.delete)
 
 
--- server.js
+++ server.js
@@ -2,10 +2,10 @@
 
 // modules ============================================================
 var express 		= require('express');
-var app			= express();
+var app				= express();
 var bodyParser		= require('body-parser');
 var methodOverride	= require('method-override');
-
+var mongoose 		= require('mongoose');
 
 // configuration ======================================================
 
