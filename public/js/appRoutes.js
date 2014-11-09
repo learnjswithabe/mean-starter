7a1863a3711ba9cc64891fc8e36573b0778647d0
Austin Lawless <austindlawless@gmail.com>
2014-11-08 19:24:47 -0600

    fixed errors

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -12,11 +12,11 @@ angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', f
 
 		// nerds page that will use the NerdController
 		.when('/nerds', {
-			templateUrl: 'views/nerds.html',
+			templateUrl: 'views/nerd.html',
 			controller: 'NerdController'
 		});
 
-	$locatoinProvider.html5Mode(true);
+	$locationProvider.html5Mode(true);
 
 }]);
 
