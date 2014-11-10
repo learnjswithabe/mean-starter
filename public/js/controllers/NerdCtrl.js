17b6f43c9b3d51d2b474e88451ddbfbc717e6685
Austin Lawless <austindlawless@gmail.com>
2014-11-09 20:47:27 -0600

    got the nerd details page going

--- public/js/controllers/NerdCtrl.js
+++ public/js/controllers/NerdCtrl.js
@@ -3,17 +3,21 @@
 'use strict';
 
 angular.module('NerdCtrl', []).controller('NerdController', function($scope, $http, $routeParams) {
+	console.log('$routeParams.nerdId=' + $routeParams.nerdId);
 	$scope.nerdId = $routeParams.nerdId;
-	// $scope.nerd;
-	// function getNerd(nerdId){
-	//	$http.get('http://localhost:8080/api/nerds/nerdId')
-	//		.success(function(data) {
-	//			$scope.nerd = data;
-	//		})
-	//		.error(function() {
-	//			console.log('unable to load nerd: nerdId=' + nerdId);
-	//		});
-	// }
-	// getNerd($scope.nerdId);
+	$scope.name = 'name';
+	function getNerd() {
+		console.log('http://localhost:8080/api/nerds/' + $routeParams.nerdId);
+		$http.get('http://localhost:8080/api/nerds/' + $routeParams.nerdId)
+			.success(function(data) {
+				console.log('data=' + data);
+				console.log('data.name=' + data.name);
+				$scope.name = data.name;
+			})
+			.error(function() {
+				console.log('unable to load nerd: nerdId=' + $routeParams.nerdId);
+			});
+	}
+	getNerd();
 });
 
