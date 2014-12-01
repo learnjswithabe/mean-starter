b6f42e2c244caf405659ab62fa5682f8eaf3d324
Austin Lawless <austindlawless@gmail.com>
2014-11-30 22:36:34 -0600

    got the list of players showing from the roster

--- public/js/controllers/PlayersCtrl.js
+++ public/js/controllers/PlayersCtrl.js
@@ -5,10 +5,21 @@
 angular.module('PlayersCtrl', []).controller('PlayersController', function($scope, $http) {
 	$scope.players = [];
 	function getPlayers(){
-		console.log('GET http://localhost:8080/api/players');
-		$http.get('http://localhost:8080/api/players')
+		console.log('GET http://localhost:8080/api/game_rosters');
+		$http.get('http://localhost:8080/api/game_rosters')
 			.success(function(data) {
-				$scope.players = data;
+				console.log('SUCCESS');
+				var rosters = data;
+				console.log('roster.length=' + rosters.length);
+				for(var i = 0; i < rosters.length; i++) {
+					var roster = rosters[i];
+					$scope.players = $scope.players.concat(roster.home_team.players);
+					$scope.players = $scope.players.concat(roster.away_team.players);
+					console.log('$scope.players.length=' + $scope.players.length);
+					for(var j = 0; j < $scope.players.length; j++) {
+						console.log('player[' + j + '].id=' + $scope.players[j].id);
+					}
+				}
 			})
 			.error(function() {
 				console.log('unable to load players');
